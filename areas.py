33317d6d7c006f8a22bd0f5b76763cdf89982f7d
JuneshG <juneshgautam5@gmail.com>
2024-10-02 21:11:06 -0500

    practicing to calculate areas in a loop code added

--- areas.py
+++ areas.py
@@ -1,23 +1,18 @@
-#define properties of different shapes
+shapes = {'square': {'function': lambda s: s*s},
+          'circle': {'function': lambda r: 3.14 * r**2},
+          'triangle': {'function': lambda b,h: 0.5*b*h}}
 
-shapes = {'square': {'formula': lambda s : s * s},
-          'circle': {'formula': lambda r : 3.14 * r * r},
-          'triangle': {'formula': lambda b, h : 0.5 * b * h}}
-
-sizes = {
-    'square': [2,4,6],
-    'circle' : [1,3,5],
-    'triangle' : [(3,4), (5,6), (7,8)]
-}
-
-# Loop through each shape and calculate the area for each size
-for shape_name, shape_props in shapes.items():
-    print(f"Shape: {shape_name}")
+sizes = {'square': [2,4,6],
+         'circle': [1,3,5],
+         'triangle': [(1,2),(3,4),(5,6)]}
+            
+for shape_name, shape_formula in shapes.items():
+    print(f'\nShape: {shape_name} ')
     for size in sizes[shape_name]:
         if shape_name == 'triangle':
-            area = shape_props['formula'](*size)
+            area = shape_formula['function'](*size)
         else:
-            area = shape_props['formula'](size)
-    
-        print(f"Size : {size}, area : {area:.2f}")
-            
+            area = shape_formula['function'](size)
+        
+        print(f'Size: {size}, area: {area}')
+
